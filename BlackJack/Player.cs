83f103620dcb60c66f043af3ca9be9d446055920
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2023-10-18 19:28:04 +0100

    feat: Add class BlackJackPlayer

--- BlackJack/Player.cs
+++ BlackJack/Player.cs
@@ -1,4 +1,59 @@
-﻿    public class Croupier: Player
+﻿namespace BlackJack
+{
+    public class Player
+    {
+        protected List<Card> hand { get; set; }
+        protected string name { get; set; }
+
+        public Player(string name)
+        {
+            hand = new List<Card>();
+            this.name = name;
+        }
+
+        public void Draw(Deck deck)
+        {
+            hand.Add(deck.Draw());
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{name}'s hand:");
+            foreach (Card card in hand) Console.WriteLine(card);
+        }
+    }
+
+    public class BlackJackPlayer: Player
+    {
+        private int score { get; set; }
+
+        public BlackJackPlayer(string name): base(name) { }
+
+        public bool HasAce()
+        {
+            foreach (Card card in hand) if (card.Value == 1) return true;
+            return false;
+        }
+
+        public void CalculateScore()
+        {
+            score = 0;
+            foreach (Card card in hand) score += card.Value;
+            if (HasAce() && score + 10 <= 21) score += 11;
+        }   
+
+        public int Score
+        {
+            get { return score; }
+            private set { score = value; }
+        }
+
+        public bool IsBust()
+        {
+            return score > 21;
+        }
+    }
+
     {
         public Croupier(): base("Croupier") { }
     }
