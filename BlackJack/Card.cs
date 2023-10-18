f74f20d378c8211870c3be49cad38db45ed91593
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2023-10-18 18:49:01 +0100

    feat: Add class Card

---
+++ BlackJack/Card.cs
@@ -0,0 +1,19 @@
+﻿namespace BlackJack
+{
+    public class Card
+    {
+        private CardSuit Suit { get; set; }
+        private int Value { get; set; }
+
+        public Card(CardSuit suit, int value)
+        {
+            Suit = suit;
+            Value = value;
+        }
+
+        public string ToString()
+        {
+            return $"{Value} of {Suit}";
+        }
+    }
+}
